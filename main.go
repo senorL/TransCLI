225a35b3a7e598f76b0cf090a0b20ebe2d66d915
tongque <2863528786@qq.com>
2024-11-25 09:22:44 +0800

    pref:对代码进行优化，可支持后续前缀树

--- main.go
+++ main.go
@@ -1,29 +1,53 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"log"
 	"strings"
 
+	"github.com/eiannone/keyboard"
 	"github.com/senorL/TransCLI/conf"
 	"github.com/senorL/TransCLI/translate"
 )
 
 func main() {
-	// 实例化一个cli应用
 	conf.GetConf()
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("\033[1;32mTrans-CLI>\033[0m ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-		input = strings.TrimRight(input, "\r\n")
+	var builder strings.Builder
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer keyboard.Close()
 
-		if input == "exit" {
-			break
+	fmt.Printf("\r\033[1;32mTrans-CLI>\033[0m %s", builder.String())
+	for {
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			log.Fatal(err)
 		}
-		translate.TranForBaidu(input)
+
+		switch key {
+		case keyboard.KeyEsc:
+			return // 按下 ESC 键退出程序
+		case keyboard.KeyEnter:
+			fmt.Printf("\n\033[1;32mTrans-CLI>\033[0m ") // 换行
+			translate.TranForBaidu(builder.String())     // 调用翻译功能
+			builder.Reset()                              // 清空输入内容
+		case keyboard.KeyBackspace, keyboard.KeyBackspace2:
+			// 删除最后一个字符
+			str := builder.String()
+			if len(str) > 0 {
+				builder.Reset()
+				builder.WriteString(str[:len(str)-1])
+			}
+		case keyboard.KeySpace:
+			builder.WriteRune(' ')
+		default:
+			builder.WriteRune(char)
+		}
+
+		// 使用 ANSI 转义序列清空当前行，然后重新打印内容
+		fmt.Printf("\r\033[1;32mTrans-CLI>\033[0m %s\033[K", builder.String())
 	}
 }
