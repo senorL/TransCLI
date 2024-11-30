e0aea7d5b30a791d244f7ca6bfb34e2765d3d77e
tongque <2863528786@qq.com>
2024-11-30 17:29:47 +0800

    feat:添加history记录，up down可以进行历史记录

--- main.go
+++ main.go
@@ -15,6 +15,9 @@ import (
 //go:embed asset/dict.txt
 var dict string
 
+var history []string
+var index int
+
 func main() {
 	conf.GetConf()
 
@@ -38,6 +41,8 @@ func main() {
 		case keyboard.KeyEnter:
 			fmt.Printf("\n\033[1;32mTrans-CLI>\033[0m ") // 换行
 			translate.TranForBaidu(builder.String())     // 调用翻译功能
+			history = append(history, builder.String())  // 记录历史
+			index = len(history) - 1                     // 更新历史记录索引
 			builder.Reset()                              // 清空输入内容
 		case keyboard.KeyBackspace, keyboard.KeyBackspace2:
 			// 删除最后一个字符
@@ -51,6 +56,19 @@ func main() {
 		case keyboard.KeyArrowDown, keyboard.KeyArrowUp, keyboard.KeyArrowLeft, keyboard.KeyArrowRight:
 			// TODO: 处理方向键
 			// 方向键控制history记录，可以进行上下翻页
+			if key == keyboard.KeyArrowDown {
+				if index < len(history)-1 {
+					index++
+					builder.Reset()
+					builder.WriteString(history[index])
+				}
+			} else if key == keyboard.KeyArrowUp {
+				if index > 0 {
+					index--
+					builder.Reset()
+					builder.WriteString(history[index])
+				}
+			}
 		default:
 			builder.WriteRune(char)
 		}
